5095a50ab39168f900ea1a171544d7513bb9da1f
Ray Miller <ray@metail.com>
2015-11-25 18:19:31 +0000

    Add Incanter dependency. Implement parse-value. Refactor main parse fn.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject thinkstats "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Think Stats - in Clojure!"
+  :url "https://github.com/ray1729/thinkstats"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [incanter "1.9.0"]])
--- src/thinkstats/dct_parser.clj
+++ src/thinkstats/dct_parser.clj
@@ -20,30 +20,51 @@
   (with-open [r (io/reader path)]
     (mapv parse-dict-line (butlast (drop 1 (line-seq r))))))
 
-(defmulti parse-value (fn [type raw-value] type))
-
-(defmethod parse-value :default [_ v] v)
+(defn parse-value
+  [type raw-value]
+  (when (not (empty? raw-value))
+    (case type
+      ("str12")          raw-value
+      ("byte" "int")     (Long/parseLong raw-value)
+      ("float" "double") (Double/parseDouble raw-value))))
 
 (defn make-row-parser
   [dict]
   (fn [row]
     (reduce (fn [accum {:keys [col type name f-len]}]
-              (let [raw-value (not-empty (str/trim (subs row col (+ col f-len))))]
+              (let [raw-value (str/trim (subs row col (+ col f-len)))]
                 (assoc accum (keyword name) (parse-value type raw-value))))
             {}
             dict)))
 
+(defn reader
+  "Open path with io/reader; coerce to a GZIPInputStream if suffix is .gz"
+  [path]
+  (if (.endsWith path ".gz")
+    (io/reader (GZIPInputStream. (io/input-stream path)))
+    (io/reader path)))
+
 (defn read-dct-data
-  [dict-path data-path]
-  (let [dict (read-dict-defn dict-path)
-        parse-fn (make-row-parser dict)]
-    (with-open [in (io/reader (GZIPInputStream. (io/input-stream data-path)))]
-      (doall (take 10 (map parse-fn (line-seq in)))))))
+  [dict rdr]
+  (let [parse-fn (make-row-parser dict)]
+    (map parse-fn (line-seq rdr))))
 
 (comment
   (def dict-path "data/2002FemPreg.dct.txt")
   (def data-path "data/2002FemPreg.dat.gz")
 
-  (read-dct-data dict-path data-path)
+  (def dict (read-dict-defn dict-path))
+
+  (def xs (with-open [r (reader data-path)]
+            (doall (read-dct-data dict r))))
+
+  (require '[clojure.pprint :refer [pprint]])
+
+  (pprint
+   (take 10 (map
+             (fn [x] (select-keys x [:caseid :prglngth :outcome :pregordr
+                                     :birthord :birthwgt_lb :birthwgt_oz
+                                     :agepreg :finalwgt]))
+                        xs)))
 
   )
